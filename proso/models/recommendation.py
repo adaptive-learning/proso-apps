437c7fddd71e2f54fae207155b8fff6d20830c32
Jan Papousek <jan.papousek@gmail.com>
2014-10-20 23:00:51 +0200

    improve score recommendation to work with parent items

--- proso/models/recommendation.py
+++ proso/models/recommendation.py
@@ -1,6 +1,7 @@
 import abc
 import random
 import math
+from collections import defaultdict
 
 
 class Recommendation:
@@ -35,17 +36,27 @@ class RandomRecommendation(Recommendation):
 
 class ScoreRecommendation(Recommendation):
 
-    def __init__(self, predictive_model, weight_probability=10.0, weight_number_of_answers=5, weight_time_ago=120, target_probability=80.0):
+    def __init__(
+            self, predictive_model, weight_probability=10.0, weight_number_of_answers=5.0,
+            weight_time_ago=120, weight_parent_time_ago=120, weight_parent_number_of_answers=2.5,
+            target_probability=80.0):
         self._predictive_model = predictive_model
         self._weight_probability = weight_probability
         self._weight_number_of_answers = weight_number_of_answers
         self._weight_time_ago = weight_time_ago
         self._target_probability = target_probability
+        self._weight_parent_time_ago = weight_parent_time_ago
+        self._weight_parent_number_of_answers = weight_parent_number_of_answers
 
     def recommend(self, environment, user, items, time, n, **kwargs):
         answers_num = dict(zip(items, environment.number_of_answers_more_items(user=user, items=items)))
         last_answer_time = dict(zip(items, environment.last_answer_time_more_items(user=user, items=items)))
         probability = dict(zip(items, self._predictive_model.predict_more_items(environment, user, items, time)))
+        parents = dict(zip(items, environment.get_items_with_values_more_items('parent', items=items)))
+        # The current implementation of retrieving features for the parent
+        # items provides only an under-approximation of the real state.
+        last_answer_time_parents = self._last_answer_time_for_parents(environment, parents, last_answer_time)
+        answers_num_parents = self._answers_num_for_parents(environment, parents, answers_num)
 
         def _score(item):
             return (
@@ -54,9 +65,31 @@ class ScoreRecommendation(Recommendation):
                 self._weight_number_of_answers * self._score_answers_num(answers_num[item]),
                 random.random()
             )
-        candidates = map(
-            lambda (s, i): i,
-            sorted(zip(map(_score, items), items), reverse=True))[:min(n, len(items))]
+
+        def _finish_score(((score, r), i)):
+            total = 0.0
+            parent_time_score = 0.0
+            parent_answers_num_score = 0.0
+            for p, v in parents[i]:
+                parent_time_score += v * self._score_last_answer_time(last_answer_time_parents[p], time)
+                parent_answers_num_score += v * self._score_answers_num(answers_num_parents[p])
+            if total > 0:
+                parent_time_score = parent_time_score / total
+                parent_answers_num_score = parent_answers_num_score / total
+            score += self._weight_parent_time_ago * parent_time_score
+            score += self._weight_parent_number_of_answers * parent_answers_num_score
+            return (score, r), i
+
+        scored = zip(map(_score, items), items)
+        candidates = []
+        while len(candidates) < n and len(scored) > 0:
+            finished = map(_finish_score, scored)
+            score, chosen = max(finished)
+            candidates.append(chosen)
+            for p, v in parents[chosen]:
+                last_answer_time_parents[p] = time
+            scored = filter(lambda (score, i): i != chosen, scored)
+
         if kwargs.get('options', False):
             rolling_success = environment.rolling_success(user=user)
             return candidates, map(
@@ -75,15 +108,39 @@ class ScoreRecommendation(Recommendation):
 
     def _score_last_answer_time(self, last_answer_time, time):
         if last_answer_time is None:
-            return 315360000
+            seconds_ago = 315360000
         else:
             seconds_ago = (time - last_answer_time).total_seconds()
-            return - 1.0 / max(seconds_ago, 0.001)
+        return - 1.0 / max(seconds_ago, 0.001)
 
     def _adjust_target_probability(self, rolling_success):
         norm = 1 - self._target_probability if rolling_success > self._target_probability else self._target_probability
         correction = ((self._target_probability - rolling_success) / max(0.001, norm)) * (1 - norm)
         return self._target_probability + correction
+
+    def _answers_num_for_parents(self, environment, parents, answers_num):
+        children = defaultdict(list)
+        for i, ps in parents.iteritems():
+            for p, v in ps:
+                children[p].append(i)
+
+        return dict(map(
+            lambda (p, chs): (p, sum(map(lambda ch: answers_num[ch], chs))),
+            children.items()))
+
+    def _last_answer_time_for_parents(self, environment, parents, last_answer_time):
+        children = defaultdict(list)
+        for i, ps in parents.iteritems():
+            for p, v in ps:
+                children[p].append(i)
+
+        def _max_time_from_items(xs):
+            times = filter(lambda x: x is not None, map(lambda x: last_answer_time[x], xs))
+            if len(times) > 0:
+                return max(times)
+            else:
+                return None
+        return dict(map(lambda (p, chs): (p, _max_time_from_items(chs)), children.items()))
 
     def _options(self, environment, item, items_with_prediction, rolling_success):
         # number of options
