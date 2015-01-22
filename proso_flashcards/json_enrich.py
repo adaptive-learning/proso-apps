c7328f1e8e16c66168793d38a21959f2085be2fc
Jan Papousek <jan.papousek@gmail.com>
2015-01-22 16:46:44 +0100

    further improvements of api for proso_flashcards

--- proso_flashcards/json_enrich.py
+++ proso_flashcards/json_enrich.py
@@ -1,11 +1,23 @@
 from django.core.cache import cache
-from proso.django.response import pass_get_parameters_string, append_get_parameters
+from proso.django.response import pass_get_parameters_string, append_get_parameters, pass_get_parameters
 from django.core.urlresolvers import reverse
 import json as json_lib
 
 
 IGNORE_GET = ['category']
 CACHE_EXPIRATION = 60 * 60 * 24 * 30
+
+def flashcards(request, json_list, nested):
+    show_flashcards_url = reverse('proso_flashcards_show_flashcards')
+    for json in json_list:
+        if nested or 'object_type' not in json:
+            continue
+        ignored_get = ['filter_column', 'filter_value'] + IGNORE_GET
+        url_options = '?filter_column={}&filter_value=' + str(json['id'])
+        url = show_flashcards_url + url_options
+        if json['object_type'] == 'category':
+            json['flashcards_url'] = pass_get_parameters(
+                request, url.format('category_id'), ignored_get)
 
 
 def url(request, json_list, nested):
--- proso_flashcards/views.py
+++ proso_flashcards/views.py
@@ -1,10 +1,12 @@
 import logging
+from django.shortcuts import get_object_or_404
 from proso.django.response import render
 import proso_common.views
 from time import time as time_lib
-from models import DecoratedAnswer
+from models import DecoratedAnswer, Category, Flashcard
 from proso.django.cache import cache_page_conditional
 import proso_common.json_enrich as common_json_enrich
+import proso_models.json_enrich as models_json_enrich
 from proso.django.request import is_user_id_overriden
 import json_enrich
 
@@ -26,12 +28,29 @@ def show_one(request, object_class, id):
     condition=lambda request: 'stats' not in request.GET and '/answers/' not in request.path_info)
 def show_more(request, object_class, should_cache=True):
     def _load_objects(request, object_class):
-        select_related_all = {}
-        prefetch_related_all = {}
+        select_related_all = {
+            DecoratedAnswer: ['general_answer']
+        }
+        prefetch_related_all = {
+            Flashcard: ['category_set'],
+            Category: ['subcategories']
+        }
         select_related = select_related_all.get(object_class, [])
         prefetch_related = prefetch_related_all.get(object_class, [])
-        objs = object_class.objects.select_related(*select_related).\
-            prefetch_related(*prefetch_related).all()
+        objs = None
+        if 'filter_column' in request.GET and 'filter_value' in request.GET:
+            column = request.GET['filter_column']
+            value = request.GET['filter_value']
+            if value.isdigit():
+                value = int(value)
+            if column == 'category_id':
+                objs = (get_object_or_404(Category, pk=value).
+                        flashcards.
+                        select_related(*select_related).
+                        prefetch_related(*prefetch_related).all())
+        if objs is None:
+            objs = object_class.objects.select_related(*select_related).\
+                prefetch_related(*prefetch_related).all()
         if object_class == DecoratedAnswer:
             if is_user_id_overriden(request):
                 user_id = int(request.GET['user'])
@@ -46,12 +65,17 @@ def show_more(request, object_class, should_cache=True):
 
 def _to_json(request, value):
     time_start = time_lib()
+    print value
     if isinstance(value, list):
         json = map(lambda x: x if isinstance(x, dict) else x.to_json(), value)
     elif not isinstance(value, dict):
         json = value.to_json()
     else:
         json = value
+    if 'stats' in request.GET:
+        common_json_enrich.enrich_by_object_type(
+            request, json, models_json_enrich.prediction, ['flashcard', 'category'])
     common_json_enrich.enrich_by_predicate(request, json, json_enrich.url, lambda x: True)
+    common_json_enrich.enrich_by_object_type(request, json, json_enrich.flashcards, ['category'])
     LOGGER.debug("converting value to simple JSON took %s seconds", (time_lib() - time_start))
     return json
