616154662d3f7c124948858770304994c249c347
Thran <exthran@gmail.com>
2015-03-19 13:14:30 +0100

    add resources for more flashcards models

--- proso_flashcards/urls.py
+++ proso_flashcards/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
-from proso_flashcards.models import Term
+from proso_flashcards.models import Term, Category, Context, Flashcard
+
+MODELS = [Term, Flashcard, Context, Category]
 
 urlpatterns = patterns(
     'proso_flashcards.views',
     url(r'^(|home)$', TemplateView.as_view(template_name="flashcards_home.html"), name='home'),
-    url(r'^term/(?P<id>\d+)$', 'show_one', {'object_class': Term}, name='show_term'),
-    url(r'^terms$', 'show_more', {'object_class': Term}, name='show_terms'),
 )
+
+for model in MODELS:
+    name = model.__name__.lower()
+    urlpatterns += patterns(
+        'proso_flashcards.views',
+        url(r'^{}/(?P<id>\d+)$'.format(name), 'show_one', {'object_class': model}, name='show_{}'.format(name)),
+        url(r'^{}s$'.format(name), 'show_more', {'object_class': model}, name='show_{}s'.format(name)),
+    )
