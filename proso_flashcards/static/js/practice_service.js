9932c3e9ae96a2f5a027f35982e1ca6198fab13d
Thran <exthran@gmail.com>
2015-04-11 11:26:02 +0200

    proso_flashcard service: avoid load again queued FC and more enhancements

--- proso_flashcards/static/js/practice_service.js
+++ proso_flashcards/static/js/practice_service.js
@@ -14,6 +14,7 @@ PracticeService = function($http, $q){
     var queue = [];
     var deferred_fc = null;
     var promise_resolved_tmp = false;
+    var current_fc = null;
 
 
     self.save_answer = function(answer){
@@ -22,12 +23,15 @@ PracticeService = function($http, $q){
 
     // return promise of flashcard
     self.get_flashcard = function(answer){
+        if(deferred_fc){
+            return $q(function(resolve, reject){
+                reject("Already one flashcard promised")
+            });
+        }
         deferred_fc  = $q.defer();
         promise_resolved_tmp = false;
         _resolve_promise();
-        deferred_fc.promise.then(function(){
-            deferred_fc = null;
-        });
+        deferred_fc.promise.then(function(){ deferred_fc = null;}, function(){ deferred_fc = null;});
         return deferred_fc.promise;
     };
 
@@ -49,12 +53,24 @@ PracticeService = function($http, $q){
 
     var _load_flashcards = function(){
         self.filter.limit  = self.fc_in_queue - queue.length;
-        if (deferred_fc && !promise_resolved_tmp) self.filter.limit ++;       // if we promised one flashcard
-        self.filter.limit = Math.min(self.filter.limit, self.fc_in_set - self.current); // check size of set
-        if (self.filter.limit == 0) return;         // nothing to do
+        if (deferred_fc && !promise_resolved_tmp) self.filter.limit ++;                  // if we promised one flashcard
+        self.filter.limit = Math.min(self.filter.limit, self.fc_in_set - self.current);  // check size of set
+        if (self.filter.limit == 0) return;                         // nothing to do
+        self.filter.avoid = current_fc ? [current_fc.id] : [];      // avoid current FC
+        queue.forEach(function(fc){
+            self.filter.avoid.push(fc.id);
+        });
 
-        // TODO ignore loaded flashcards
-        $http.get("/flashcards/practice/", {params: self.filter})
+        var filter = {};
+        for (var key in self.filter){
+            if (self.filter[key] instanceof Array) {
+                filter[key] = JSON.stringify(self.filter[key]);
+            }else{
+                filter[key] = self.filter[key];
+            }
+        }
+
+        $http.get("/flashcards/practice/", {params: filter})
             .success(function(response){
                 queue = queue.concat(response.data.flashcards);
                 _resolve_promise();
@@ -74,9 +90,10 @@ PracticeService = function($http, $q){
             return;
         }
         if (queue.length > 0) {
-            deferred_fc.resolve(queue.shift());
+            current_fc = queue.shift();
             self.current++;
             promise_resolved_tmp = true;
+            deferred_fc.resolve(current_fc);
         }
         _load_flashcards();
     };
