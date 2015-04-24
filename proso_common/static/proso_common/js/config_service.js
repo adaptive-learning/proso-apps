f49e9d59578223e34373d4c091c267fab2e57469
Thran <exthran@gmail.com>
2015-04-24 15:18:58 +0200

    proso_common configService: debug mode and faking values with GET parameters

--- proso_common/static/proso_common/js/config_service.js
+++ proso_common/static/proso_common/js/config_service.js
@@ -2,8 +2,15 @@ try{ m = angular.module('proso_apps.services'); } catch (err) { m = angular.modu
 m.service("configService", ["$http", function($http){
     var self = this;
     var config = null;
+    var GET;
 
     self.get_config = function(app_name, key, default_value){
+        if (GET[app_name + "." + key]) {
+            variable = GET[app_name + "." + key];
+            if (GET.debug) { console.log(app_name + "." + key, "fake from url", variable); }
+            return variable;
+        }
+
         if (config === null){
             console.error("Config not loaded");
             return;
@@ -12,10 +19,16 @@ m.service("configService", ["$http", function($http){
         var variable = config[app_name];
         var path =  key.split(".");
         for (var i=0; i < path.length; i++){
-            if (typeof variable === 'undefined'){ return default_value;}
+            if (typeof variable === 'undefined'){
+                if (GET.debug) { console.log(app_name + "." + key, "use default", default_value); }
+                return default_value;
+            }
             variable = variable[path[i]];
         }
-        if (typeof variable === 'undefined'){ return default_value;}
+        if (typeof variable === 'undefined'){
+            if (GET.debug) { console.log(app_name + "." + key, "use default", default_value); }
+            return default_value;}
+        if (GET.debug) { console.log(app_name + "." + key, "from config", variable); }
         return variable;
     };
 
@@ -32,4 +45,21 @@ m.service("configService", ["$http", function($http){
     self.process_config = function(data){
         config = angular.copy(data);
     };
+
+    var parse_GET = function(){
+        GET = getUrlVars();
+    };
+
+    parse_GET();
 }]);
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
