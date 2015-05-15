be0ebdba555863b0d292e8b67e53b42c29d47a6e
Thran <exthran@gmail.com>
2015-05-15 13:44:22 +0200

    add cache decorator for functions with deterministic results (pure) - resolve #69

--- proso/django/util.py
+++ proso/django/util.py
@@ -1,5 +1,9 @@
 from functools import wraps
+import hashlib
+import logging
+from django.core.cache import cache
 
+LOGGER = logging.getLogger('django.request')
 
 def disable_for_loaddata(signal_handler):
     @wraps(signal_handler)
@@ -7,4 +11,26 @@ def disable_for_loaddata(signal_handler):
         if kwargs.get('raw'):
             return
         signal_handler(*args, **kwargs)
+    return wrapper
+
+
+def cache_pure(f, expiration=60*60*24*30):
+    """ Cache decorator for functions taking one or more arguments. """
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        print args
+        key = "{}:args:{}-kwargs:{}".format(f.__name__, repr(args), repr(kwargs))
+        hash = hashlib.sha1(key).hexdigest()
+        if hash in cache:
+            LOGGER.debug("Loaded function result form CACHE; key: %s, hash %s", key, hash)
+            return cache.get(hash)
+
+        print key, args
+
+        value = f(*args, **kwargs)
+        LOGGER.debug("Saved function result to CACHE; key: %s, hash %s", key, hash)
+        cache.set(hash, value, expiration)
+
+        return value
+
     return wrapper
