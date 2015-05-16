4004099bc32917cec8e164293d1311d8e60e7d2a
Thran <exthran@gmail.com>
2015-05-16 17:13:31 +0200

    handle self argument in cache decorator

--- proso/django/util.py
+++ proso/django/util.py
@@ -1,9 +1,11 @@
 from functools import wraps
 import hashlib
 import logging
+import re
 from django.core.cache import cache
 
 LOGGER = logging.getLogger('django.request')
+
 
 def disable_for_loaddata(signal_handler):
     @wraps(signal_handler)
@@ -11,14 +13,21 @@ def disable_for_loaddata(signal_handler):
         if kwargs.get('raw'):
             return
         signal_handler(*args, **kwargs)
+
     return wrapper
 
 
-def cache_pure(f, expiration=60*60*24*30):
+def cache_pure(f, expiration=60 * 60 * 24 * 30):
     """ Cache decorator for functions taking one or more arguments. """
+
     @wraps(f)
     def wrapper(*args, **kwargs):
-        key = "{}:args:{}-kwargs:{}".format(f.__name__, repr(args), repr(kwargs))
+        if len(args) > 0 and re.match(r"<.+ object at \w+>", repr(args[0])) is not None:
+            key_args = [args[0].__class__] + list(args[1:])
+        else:
+            key_args = args
+
+        key = "{}:args:{}-kwargs:{}".format(f.__name__, repr(key_args), repr(kwargs))
         hash = hashlib.sha1(key).hexdigest()
         if hash in cache:
             value = cache.get(hash)
