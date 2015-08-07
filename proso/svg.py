8cf6008a0e2256003ba314755b1f7fb1221d5c4d
Jan Papousek <jan.papousek@gmail.com>
2015-08-07 15:34:42 +0200

    do not bother with doctype

--- proso/svg.py
+++ proso/svg.py
@@ -23,10 +23,7 @@ class Printer:
             f.write(str(self))
 
     def __str__(self):
-        return """<?xml version="1.0" standalone="no"?>
-        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
-        "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-        <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        return """<svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
         {}
         </svg>
         """.format(self._output)
