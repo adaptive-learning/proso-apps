4864053f96bb161859d149c014e0867aa0aee255
Vit Stanislav <slaweet@seznam.cz>
2015-08-28 09:41:04 +0200

    Add FlashcardAnswer to admin

--- proso_flashcards/admin.py
+++ proso_flashcards/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import Term, Context, Flashcard, Category
+from models import Term, Context, Flashcard, Category, FlashcardAnswer
+from proso_models.admin import pretty_date
 
 
 class TermAdmin(admin.ModelAdmin):
@@ -22,7 +23,33 @@ class CategoryAdmin(admin.ModelAdmin):
     raw_id_fields = ('terms', 'subcategories', 'flashcards', 'contexts')
 
 
+class AnswerAdmin(admin.ModelAdmin):
+
+    def is_correct(self, a):
+        return a.item_answered == a.item_asked
+    is_correct.short_description = 'Correct'
+    is_correct.boolean = True
+
+    def asked_ago(self, a):
+        return pretty_date(a.time)
+    asked_ago.short_description = 'When Asked'
+
+    def options_count(self, a):
+        return a.options.count()
+
+    list_display = (
+        'user',
+        'item_asked',
+        'item_answered',
+        'is_correct',
+        'direction',
+        'options_count',
+        'asked_ago')
+    raw_id_fields = ("options",)
+
+
 admin.site.register(Term, TermAdmin)
 admin.site.register(Context, ContextAdmin)
 admin.site.register(Flashcard, FlashcardAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(FlashcardAnswer, AnswerAdmin)
