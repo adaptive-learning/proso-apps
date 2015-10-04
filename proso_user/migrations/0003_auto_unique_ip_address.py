31ea7997307c37e9ab4380843aaaee37153290ad
Jan Papousek <jan.papousek@gmail.com>
2015-10-04 11:19:14 +0200

    proso_user: make ip address unique for location

---
+++ proso_user/migrations/0003_auto_unique_ip_address.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('proso_user', '0002_auto_unique_content_hash'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='location',
+            name='ip_address',
+            field=models.CharField(default=None, max_length=39, unique=True, null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
