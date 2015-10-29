adaptive-learning/proso-apps

commit dd6c61c462894ea7a5dcc4e291a1175802e0c1ba

prevent race condition caused by cache expiration

proso/django/util.py
@@ -6,6 +6,7 @@ from django.core.cache import cache
 from django.db import connection
 
 LOGGER = logging.getLogger('django.request')
+CACHE_MISS = 'proso-apps-cache-miss'
 
 
 def disable_for_loaddata(signal_handler):
@@ -29,9 +30,9 @@ def cache_pure(f, expiration=60 * 60 * 24 * 30):
             key_args = args
 
         key = "{}:args:{}-kwargs:{}".format(f.__name__, repr(key_args), repr(kwargs))
-        hash = hashlib.sha1(key).hexdigest()
-        if hash in cache:
-            value = cache.get(hash)
+        hash_key = hashlib.sha1(key).hexdigest()
+        value = cache.get(hash, CACHE_MISS)
+        if value != CACHE_MISS:
             LOGGER.debug("loaded function result (%s...) form CACHE; key: %s..., hash %s", str(value)[:300], key[:300], hash)
             return value
 

commit b858147e49c54e086b58a0109ba1529fa6372253

fix naming bug introduced in dd6c61c462894ea7a5dcc4e291a1175802e0c1ba

proso/django/util.py
@@ -31,14 +31,14 @@ def cache_pure(f, expiration=60 * 60 * 24 * 30):
 
         key = "{}:args:{}-kwargs:{}".format(f.__name__, repr(key_args), repr(kwargs))
         hash_key = hashlib.sha1(key).hexdigest()
-        value = cache.get(hash, CACHE_MISS)
+        value = cache.get(hash_key, CACHE_MISS)
         if value != CACHE_MISS:
-            LOGGER.debug("loaded function result (%s...) form CACHE; key: %s..., hash %s", str(value)[:300], key[:300], hash)
+            LOGGER.debug("loaded function result (%s...) form CACHE; key: %s..., hash %s", str(value)[:300], key[:300], hash_key)
             return value
 
         value = f(*args, **kwargs)
-        LOGGER.debug("saved function result (%s...) to CACHE; key: %s..., hash %s", str(value)[:300], key[:300], hash)
-        cache.set(hash, value, expiration)
+        LOGGER.debug("saved function result (%s...) to CACHE; key: %s..., hash %s", str(value)[:300], key[:300], hash_key)
+        cache.set(hash_key, value, expiration)
 
         return value
 