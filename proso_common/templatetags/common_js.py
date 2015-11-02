6a7924904cc971f92af476512f0cd80445c8b791
Jan Papousek <jan.papousek@gmail.com>
2015-11-02 16:07:47 +0100

    remove obsolete print

--- proso_common/templatetags/common_js.py
+++ proso_common/templatetags/common_js.py
@@ -11,6 +11,5 @@ def js_files():
     js_files = []
     if hasattr(settings, 'PROSO_JS_FILES'):
         for f in settings.PROSO_JS_FILES:
-            print f, static(f)
             js_files.append('<script src="{}"></script>'.format(static(f)))
     return "\n".join(js_files)
