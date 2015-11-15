24ff8788b43b6b919f65d08f4ff4544f23df9fc1
Jan Papousek <jan.papousek@gmail.com>
2015-11-15 12:21:07 +0100

    permanent values should not reference info
    issues: #98

---
+++ proso_models/migrations/0010_permanent_values.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.db import connection
+from contextlib import closing
+
+
+def remove_info_id(apps, schema_editor):
+    with closing(connection.cursor()) as cursor:
+        cursor.execute('UPDATE proso_models_variable SET info_id = NULL WHERE permanent')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('proso_models', '0009_remove_ab'),
+    ]
+
+    operations = [
+        migrations.RunPython(remove_info_id)
+    ]
