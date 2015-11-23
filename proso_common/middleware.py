5f24f8521ee4222df938c2f1861a0fd8e572badd
Vit Stanislav <slaweet@seznam.cz>
2015-11-23 08:33:05 +0100

    Add middleware for language in domain

--- proso_common/middleware.py
+++ proso_common/middleware.py
@@ -9,8 +9,10 @@ from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 from proso.django.response import HttpError, render_json
+import datetime
+import logging
 
-
+LOGGER = logging.getLogger('django.request')
 _HTML_TYPES = ('text/html', 'application/xhtml+xml')
 
 
@@ -75,3 +77,67 @@ class LanguageInPathMiddleware(object):
             url = request.path
             url = url.replace('/' + language_code, '')
             return HttpResponseRedirect(url)
+
+
+def redirect_domain(request, target_domain):
+    url = ('http://' + target_domain + request.get_full_path() +
+           '?sessionid=' + request.COOKIES.get('sessionid', ''))
+    return HttpResponseRedirect(url)
+
+
+def set_lang(request, language_code):
+    translation.activate(language_code)
+    request.LANGUAGE_CODE = translation.get_language()
+    request.COOKIES[settings.LANGUAGE_COOKIE_NAME] = language_code
+    request.session[translation.LANGUAGE_SESSION_KEY] = language_code
+
+
+class LanguageInDomainMiddleware(object):
+    def __init__(self):
+        self.language_codes = set(dict(settings.LANGUAGES).keys())
+
+    def process_request(self, request):
+        language_code = translation.get_language()
+        target_domain = settings.LANGUAGE_DOMAINS[language_code]
+        if target_domain != request.META['HTTP_HOST']:
+            domain_to_lang_dict = dict((v, k) for k, v in settings.LANGUAGE_DOMAINS.iteritems())
+            language_code = domain_to_lang_dict.get(request.META['HTTP_HOST'])
+            if language_code is not None:
+                set_lang(request, language_code)
+            else:
+                LOGGER.error(
+                    'LanguageInDomainMiddleware: invalid HTTP_HOST: {}'.format(
+                        request.META['HTTP_HOST']))
+
+        language_code = request.path_info.lstrip('/').split('/', 1)[0]
+        if language_code in self.language_codes:
+            target_domain = settings.LANGUAGE_DOMAINS[language_code]
+            if target_domain != request.META['HTTP_HOST']:
+                return redirect_domain(request, target_domain)
+            if request.COOKIES.get('sessionid', 'None') == request.GET.get('sessionid', ''):
+                # TODO: remove get param
+                pass
+            set_lang(request, language_code)
+            url = request.get_full_path()
+            url = url.replace('/' + language_code, '')
+            return HttpResponseRedirect(url)
+
+    def process_response(self, request, response):
+        if request.method == 'GET' and 'sessionid' in request.GET:
+            max_age = 7 * 24 * 60 * 60
+            expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age)
+            response.set_cookie('sessionid', request.GET['sessionid'], expires=expires)
+        return response
+
+
+class GoogleAuthChangeDomain(object):
+    def process_request(self, request):
+        if request.path_info == '/login/google-oauth2/':
+            auth_domain = settings.AUTH_DOMAIN
+            if auth_domain != request.META['HTTP_HOST']:
+                return redirect_domain(request, auth_domain)
+        if request.path_info == '/complete/google-oauth2/':
+            language_code = translation.get_language()
+            target_domain = settings.LANGUAGE_DOMAINS[language_code]
+            if target_domain != request.META['HTTP_HOST']:
+                return redirect_domain(request, target_domain)
