0a3750163878170fe9fe65599fe9e53f38bb9073
Jan Papousek <jan.papousek@gmail.com>
2016-01-26 09:23:46 +0100

    simple way to iterate over data frame

---
+++ proso/dfutil.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+class DictIterator:
+
+    def __init__(self, dataframe):
+        self._dataframe = dataframe
+
+    def __iter__(self):
+        self._iter = self._dataframe.values.__iter__()
+        self._columns = self._dataframe.columns.values
+        return self
+
+    def __len__(self):
+        return len(self._dataframe)
+
+    def next(self):
+        return dict(zip(self._columns, self._iter.next()))
+
+
+def iterdicts(dataframe):
+    """
+    Allows you to efficiently iterate over the dataframe.
+
+    Args:
+        dataframe (pandas.DataFrame):
+            dataframe to iterate
+
+    Returns:
+        iterator allowing you to iterate over the dataframe providing rows as
+        dicts
+    """
+    return DictIterator(dataframe)
