d404ea89da2224cc4db6890972e6ead489abdd9e
Thran <exthran@gmail.com>
2016-01-29 20:15:48 +0100

    fix problem with escaping

--- proso_common/templatetags/common_js.py
+++ proso_common/templatetags/common_js.py
@@ -1,6 +1,7 @@
 from django import template
 from django.templatetags.static import static
 from django.conf import settings
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -12,4 +13,4 @@ def js_files():
     if hasattr(settings, 'PROSO_JS_FILES'):
         for f in settings.PROSO_JS_FILES:
             js_files.append('<script src="{}"></script>'.format(static(f)))
-    return "\n".join(js_files)
+    return mark_safe("\n".join(js_files))
