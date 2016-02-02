57df90e58ca5437b13c37d09af990cbbe9344ead
Jan Papousek <jan.papousek@gmail.com>
2016-02-02 09:29:32 +0100

    remove obsolete proso_questions*

--- proso_questions_client/static/bower_components/angular-google-experiments/README.md
+++
@@ -1,4 +0,0 @@
-angular-google-experiments
-==========================
-
-Angular module that interfaces with Google Analytics Experiments, change your content dynamically.
--- proso_questions_client/static/jstpl/hash.js
+++
@@ -1,9 +0,0 @@
-;(function(window, undefined){
-    var hashes = {{hashes}};
-    function get_hash(filename) {
-        return hashes[filename] ? hashes[filename] : "";
-    }
-    window.hash = function(filename) {
-        return filename + "?hash=" + get_hash(filename);
-    }
-})(window);
