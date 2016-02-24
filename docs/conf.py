8582ad4c9b59df2b149830ae0774647061c9f8dd
Jan Papousek <jan.papousek@gmail.com>
2016-02-24 13:26:46 +0100

    set environment variable in conf.py instead of Makefile
    issues: #113

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,8 @@ import os
 import proso.release
 import mock
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'testproject.settings'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
