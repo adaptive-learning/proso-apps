8656796d5d876cfadb8ab90c04294fb29f73235f
Jan Papousek <jan.papousek@gmail.com>
2016-03-01 13:04:49 +0100

    make tests for load_user_questions command stable

--- proso/django/test.py
+++ proso/django/test.py
@@ -6,3 +6,13 @@ class TestCase(django.test.TestCase):
     def setUp(self):
         self.client = django.test.Client()
         self.client.logout()
+
+    def _remove_ids(self, value):
+        if isinstance(value, dict):
+            if 'id' in value:
+                del value['id']
+            return {k: self._remove_ids(v) for (k, v) in value.items()}
+        elif isinstance(value, list):
+            return [self._remove_ids(v) for v in value]
+        else:
+            return value
--- proso_user/commands_test.py
+++ proso_user/commands_test.py
@@ -1,27 +1,27 @@
 from django.core.management import call_command
-from django.test import TestCase
+from proso.django.test import TestCase
 from proso_user.models import UserQuestion
 
 
 class TeadQuestionsLoading(TestCase):
 
     def test_load_configab_experiments(self):
+        self.maxDiff = None
         call_command('load_user_questions', 'testproject/test_data/user/user_questions.json')
         expected = {
-            'content': 'Jaké je tvé pohlaví?',
-            'id': 1,
-            'answer_type': 'c',
-            'lang': 'cs',
             'active': True,
-            'possible_answers': [
-                {'content': 'Muž', 'object_type': 'user_question_possible_answer', 'id': 1, 'identifier': 'male'},
-                {'content': 'Žena', 'object_type': 'user_question_possible_answer', 'id': 2, 'identifier': 'female'},
-                {'content': 'Neuvedeno', 'object_type': 'user_question_possible_answer', 'id': 3, 'identifier': 'not_specified'}
-            ],
-            'on_events': [{'id': 1, 'object_type': 'user_question_event', 'type': 'practice_sequence_finished', 'value': '5'}],
+            'answer_type': 'c',
+            'conditions': [{'object_type': 'user_question_condition', 'type': 'user_status', 'value': 'registered'}],
+            'content': 'What is your sex?',
             'identifier': 'sex',
+            'lang': 'en',
             'object_type': 'user_question',
-            'conditions': [{'id': 1, 'object_type': 'user_question_condition', 'type': 'user_status', 'value': 'registered'}],
+            'on_events': [{'object_type': 'user_question_event', 'type': 'practice_sequence_finished', 'value': '5'}],
+            'possible_answers': [
+                {'content': 'Male', 'identifier': 'male', 'object_type': 'user_question_possible_answer'},
+                {'content': 'Female', 'identifier': 'female', 'object_type': 'user_question_possible_answer'},
+                {'content': 'Not specified', 'identifier': 'not_specified', 'object_type': 'user_question_possible_answer'}
+            ],
             'repeat': False
         }
-        self.assertEqual(UserQuestion.objects.all()[0].to_json(), expected)
+        self.assertEqual(self._remove_ids(UserQuestion.objects.filter(lang='en')[0].to_json()), expected)
