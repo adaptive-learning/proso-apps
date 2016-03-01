02b96b72a4090bc8ef46bbf344c566120e74f429
Jan Papousek <jan.papousek@gmail.com>
2016-03-01 11:11:00 +0100

    test some commands
    issues: #129

---
+++ proso_configab/commands_test.py
@@ -0,0 +1,9 @@
+from django.core.management import call_command
+from django.test import TestCase
+from proso_configab.models import Experiment
+
+class TeadExperimentsLoading(TestCase):
+
+    def test_load_configab_experiments(self):
+        call_command('load_configab_experiments', 'testproject/test_data/configab/ab_experiments.json')
+        self.assertEqual(Experiment.objects.get(is_enabled=True).identifier, 'options-construction')
