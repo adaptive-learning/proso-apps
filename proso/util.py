27e1f5f4d799bbcb464654781f9ab882815dc745
Jan Papousek <jan.papousek@gmail.com>
2016-03-15 13:37:33 +0100

    introduce timeit decorator

--- proso/util.py
+++ proso/util.py
@@ -1,6 +1,12 @@
-import re
 import importlib
+import logging
+import re
 import time
+from functools import wraps
+
+
+LOGGER = logging.getLogger('django.request')
+
 
 _timers = {}
 
@@ -12,6 +18,27 @@ def timer(name):
         diff = now - _timers[name]
     _timers[name] = now
     return diff
+
+
+class timeit:
+
+    def __init__(self, name=None):
+        if name is None:
+            name = 'unknown'
+        self._name = name
+
+    def __call__(self, function):
+
+        @wraps(function)
+        def timed(*args, **kw):
+            ts = time.time()
+            result = function(*args, **kw)
+            te = time.time()
+
+            LOGGER.debug('%s: %r (%r, %r) took %2.2f seconds' % (self._name, function.__name__, args, kw, te - ts))
+            return result
+
+        return timed
 
 
 def instantiate(classname, *args, **kwargs):
