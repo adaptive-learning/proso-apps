a49d6b77bc85687939698515d5a5f8373915d5b1
Thran <exthran@gmail.com>
2016-05-09 15:56:11 +0200

    edookit login - refresh access token, update user info

--- proso/django/edookit.py
+++ proso/django/edookit.py
@@ -1,6 +1,7 @@
 import json
 from urllib.request import urlopen, Request
-
+import time
+from social.apps.django_app.default.models import UserSocialAuth
 from social.backends.oauth import BaseOAuth2
 
 
@@ -24,6 +25,11 @@ class EdookitOAuth2(BaseOAuth2):
         ('gender', 'gender'),
     ]
 
+    def extra_data(self, user, uid, response, details=None, *args, **kwargs):
+        data = super(EdookitOAuth2, self).extra_data(user, uid, response, details, *args, **kwargs)
+        data['expires_at'] = data['expires_in'] + time.time() - 1
+        return data
+
     def get_user_details(self, response):
         """Return user details from Edookit account"""
         return {
@@ -40,3 +46,73 @@ class EdookitOAuth2(BaseOAuth2):
         request.add_header("Authorization", "Bearer {}".format(access_token))
         response = urlopen(request)
         return json.loads(response.read().decode('utf8'))
+
+
+def get_edookit_social_user(user):
+    """ Get user social auth object (with edookit provider). If social user do not exists raises exception
+
+    Args:
+        user (User): django user object
+
+    Returns:
+        social user (UserSocialAuth)
+    """
+    if not hasattr(user, 'social_auth'):
+        raise ValueError('User is not is not sign in.')
+    edookit_users = user.social_auth.filter(provider='edookit')
+    if len(edookit_users) == 0:
+        raise ValueError('User is not paired with Edookit account.')
+    if len(edookit_users) > 1:
+        raise ValueError('User is associated with multiple Edookit accounts.')
+    return edookit_users[0]
+
+
+def refresh_token(edookit_user):
+    """ Refresh access token
+
+    Args:
+        edookit_user (UserSocialAuth):
+
+    Returns:
+        extra_data (dict): extra data with new token
+    """
+    backend = edookit_user.get_backend_instance()
+    new_data = backend.refresh_token(edookit_user.extra_data['refresh_token'])
+    new_data['expires_at'] = new_data['expires_in'] + time.time() - 1
+    edookit_user.extra_data.update(new_data)
+    edookit_user.save()
+    return edookit_user.extra_data
+
+
+def get_access_token(user):
+    """Get access token of user. Refreshes it if expired.
+
+    Args:
+        user (UserSocialAuth or User):
+
+    Returns:
+        access_token (str)
+    """
+    edookit_user = user if type(user) == UserSocialAuth else get_edookit_social_user(user)
+    data = edookit_user.extra_data
+    if data['expires_at'] <= time.time():
+        return refresh_token(edookit_user)["access_token"]
+    return data["access_token"]
+
+
+def update_user_data(user):
+    """Update extra data form Edookit OAuth
+
+    Args:
+        user (User): django user
+
+    Returns:
+        extra_data (dict): new updated data
+    """
+    edookit_user = get_edookit_social_user(user)
+    backend = edookit_user.get_backend_instance()
+    data = edookit_user.extra_data
+    new_data = backend.user_data(access_token=get_access_token(edookit_user))
+    data.update(new_data)
+    edookit_user.save()
+    return new_data
