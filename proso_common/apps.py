9f36fda6ae3ca5ea01d480bd5f959b607d64a0dd
Jan Papousek <jan.papousek@gmail.com>
2016-08-01 15:56:05 +0200

    add some middlewares automatically

---
+++ proso_common/apps.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig as OAppConfig
+from django.conf import settings
+
+
+MIDDLEWARE_CLASSES = [
+    'proso_common.models.CommonMiddleware',
+    'proso_common.middleware.ToolbarMiddleware',
+    'proso_common.middleware.ErrorMiddleware',
+    'proso_common.middleware.AuthAlreadyAssociatedMiddleware',
+
+]
+
+
+class AppConfig(OAppConfig):
+
+    name = 'proso_common'
+
+    def ready(self):
+        for middleware in MIDDLEWARE_CLASSES:
+            settings.MIDDLEWARE_CLASSES = settings.MIDDLEWARE_CLASSES + (middleware, )
