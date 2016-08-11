30578845d635978b9346f4ba038c449e24357aee
Jan Papousek <jan.papousek@gmail.com>
2016-08-11 23:01:10 +0200

    fix random config assignment

--- proso_configab/assignment.py
+++ proso_configab/assignment.py
@@ -14,7 +14,7 @@ class RandomStrategy(Strategy):
     def assign_setups(self, setups_by_experiment):
         result = []
         for experiment_id, setups in setups_by_experiment.items():
-            chosen_id = roulette({s.id: s.probability for s in setups})[0]
+            chosen_id = roulette({s.id: s.probability for s in setups}, 1)[0]
             chosen_setup = [s for s in setups if s.id == chosen_id][0]
             result.append(chosen_setup)
         return result
