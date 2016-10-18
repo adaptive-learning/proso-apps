eac5377e2e660e4965e9219e7d736c9f3084d6a2
Jan Papousek <jan.papousek@gmail.com>
2016-10-18 15:49:58 +0200

    hack: fix dependency on django

--- proso/models/context.py
+++ proso/models/context.py
@@ -1,20 +1,34 @@
-from proso_common.models import custom_config_filter
-from proso_models.models import Item
+from django.core.exceptions import ImproperlyConfigured
+
+DJANGO_READY = False
+
+try:
+    from proso_common.models import custom_config_filter
+    from proso_models.models import Item
+    DJANGO_READY = True
+except ImproperlyConfigured:
+    pass
 
 
 class selected_item_context:
 
     def __init__(self, item, all_items):
-        parents = Item.objects.get_reachable_parents(all_items)[item]
+        global DJANGO_READY
+        if DJANGO_READY:
+            parents = Item.objects.get_reachable_parents(all_items)[item]
 
-        def _custom_config_filter(key, value):
-            if key != 'selected_item_has_parent':
-                return None
-            return int(value) in parents
-        self._context = custom_config_filter(_custom_config_filter)
+            def _custom_config_filter(key, value):
+                if key != 'selected_item_has_parent':
+                    return None
+                return int(value) in parents
+            self._context = custom_config_filter(_custom_config_filter)
+        else:
+            self._context = None
 
     def __enter__(self):
-        self._context.__enter__()
+        if self._context is not None:
+            self._context.__enter__()
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self._context.__exit__(exc_type, exc_value, traceback)
+        if self._context is not None:
+            self._context.__exit__(exc_type, exc_value, traceback)
