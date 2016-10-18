0f0acc625c30c93dbb1fe02abf7fec2b64f8a5de
Jan Papousek <jan.papousek@gmail.com>
2016-10-18 11:20:59 +0200

    be able to create an instance with lazy parameters

--- proso/reflection.py
+++ proso/reflection.py
@@ -1,4 +1,5 @@
 import importlib
+import inspect
 import re
 
 
@@ -15,8 +16,30 @@ def instantiate(classname, *args, **kwargs):
         >>> instantiate('collections.defaultdict')
         defaultdict(None, {})
     """
+    return get_class(classname)(*args, **kwargs)
+
+
+def instantiate_with_lazy_parameters(classname, kwarg_fun, **kwargs):
+    clazz = get_class(classname)
+    arg_spec = inspect.getargspec(clazz.__init__)
+    args = arg_spec.args[1:]
+    args_default = dict(zip(args[-len(arg_spec.defaults):], arg_spec.defaults))
+    args_default.update(kwargs)
+
+    def _kwarg_fun(arg):
+        if arg not in args_default:
+            return lambda: kwarg_fun(arg)
+        else:
+            return lambda: kwarg_fun(arg, args_default[arg])
+    return clazz(**{
+        arg: _kwarg_fun(arg)
+        for arg in args
+    })
+
+
+def get_class(classname):
     matched = re.match('(.*)\.(\w+)', classname)
     if matched is None:
-        raise Exception('can instantiate only class with packages: %s' % classname)
+        raise Exception('can found only class with packages: %s' % classname)
     module = importlib.import_module(matched.groups()[0])
-    return getattr(module, matched.groups()[1])(*args, **kwargs)
+    return getattr(module, matched.groups()[1])
