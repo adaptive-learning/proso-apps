592b524cff6065e994e72e85502987104559cc4e
Jan Papousek <jan.papousek@gmail.com>
2016-10-30 10:58:03 +0100

    add forgotten proso.conversion module

---
+++ proso/conversion.py
@@ -0,0 +1,23 @@
+import json
+
+
+def str2type(value):
+    """
+    Take a string and convert it to a value of proper type.
+
+
+        .. testsetup::
+
+            from proso.coversion import str2type
+
+        .. doctest::
+
+            >>> print(str2type("[1, 2, 3]")
+            [1, 2, 3]
+    """
+    if not isinstance(value, str):
+        return value
+    try:
+        return json.loads(value)
+    except json.decoder.JSONDecodeError:
+        return value
