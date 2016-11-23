c8656e92f371542af0682d00111399e3066b0cad
Vít Stanislav <slaweet@gmail.com>
2016-11-23 12:04:34 +0100

    Add payment status filter to subscripitons admin

--- proso_subscription/admin.py
+++ proso_subscription/admin.py
@@ -2,6 +2,28 @@
 
 from .models import Subscription, DiscountCode
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+
+class PaymentStatusListFilter(admin.SimpleListFilter):
+    title = _('Payment status')
+    parameter_name = 'payment_status'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('PAID', _('Paid')),
+            ('FREE', _('Free')),
+            ('TIMEOUTED', _('Timeouted')),
+            ('CREATED', _('Created')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'FREE':
+            return queryset.filter(payment__isnull=True)
+        elif self.value() is not None:
+            return queryset.filter(payment__state=self.value())
+        else:
+            return queryset
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -18,6 +40,7 @@ class SubscriptionAdmin(admin.ModelAdmin):
         'user__username',
     )
     list_filter = (
+        PaymentStatusListFilter,
         'plan_description',
         'discount',
     )
