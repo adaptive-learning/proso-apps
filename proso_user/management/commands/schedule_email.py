071d5214601c8c0a412eb7073dd803bd9c620986
Jan Papousek <jan.papousek@gmail.com>
2016-11-28 22:16:18 +0100

    improve schedule_emails command

--- proso_user/management/commands/schedule_email.py
+++ proso_user/management/commands/schedule_email.py
@@ -28,7 +28,9 @@ class Command(BaseCommand):
         ),
         make_option(
             '--subject',
-            dest='subject'
+            dest='subjects',
+            action='append',
+            default=[]
         ),
         make_option(
             '--lang',
@@ -46,21 +48,49 @@ class Command(BaseCommand):
             dest='dry',
             action='store_true',
             default=False
+        ),
+        make_option(
+            '--active-since',
+            dest='active_since',
+            default=None
         )
     )
 
     def handle(self, *args, **options):
-        if options['template_file'] is None or options['subject'] is None or options['from_email'] is None:
+        if options['template_file'] is None or len(options['subjects']) == 0 or options['from_email'] is None:
             raise CommandError('Template file, subject and FROM e-mail have to be specified.')
-        users = ScheduledEmail.objects.schedule_more(
-            options['from_email'], options['subject'], os.path.realpath(options['template_file']),
-            emails=None if len(options['emails']) == 0 else options['emails'],
-            skip_emails=None if len(options['skip_emails']) == 0 else options['skip_emails'],
-            langs=None if len(options['langs']) == 0 else options['langs'],
-            output_dir=None if options['output_dir'] is None else os.path.realpath(options['output_dir']),
-            dry=options['dry']
-        )
-        if options['verbosity'] > 1:
-            print('E-mail scheduled for:')
+        if len(options['langs']) > 0:
+            subjects = {sub.split(':')[0]: ':'.join(sub.split(':')[1:]) for sub in options['subjects']}
+            for lang in options['langs']:
+                if lang not in subjects:
+                    raise CommandError('There is no subject for language "{}", please specify it as --subject <LANG>:<SUBJECT>.'.format(lang))
+            to_process = [(lang, subjects[lang], options['template_file'].format(lang)) for lang in options['langs']]
+        else:
+            template_file = options['template_file']
+            subject = options['subjects'][0]
+            to_process = [(None, subject, template_file)]
+        skip_emails = list(options['skip_emails'])
+        for lang, subject, template_file in to_process:
+            if options['dry']:
+                print()
+            users = ScheduledEmail.objects.schedule_more(
+                options['from_email'],
+                subject,
+                os.path.realpath(template_file),
+                emails=None if len(options['emails']) == 0 else options['emails'],
+                skip_emails=None if len(skip_emails) == 0 else skip_emails,
+                langs=None if lang is None else [lang],
+                output_dir=None if options['output_dir'] is None else os.path.realpath(options['output_dir']),
+                dry=options['dry'],
+                active_since=options['active_since']
+            )
             for user in users:
-                print('    ', user.email)
+                skip_emails.append(user.email)
+            if options['verbosity'] > 1:
+                print()
+                if len(users) == 0:
+                    print('No e-mail [{}] "{}" scheduled'.format(subject, template_file))
+                else:
+                    print('E-mail [{}] "{}" scheduled for:'.format(subject, template_file))
+                for user in users:
+                    print('    ', user.email)
