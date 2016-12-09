6446e82b292d1256093637c64abb71a51a755ca2
Jan Papousek <jan.papousek@gmail.com>
2016-12-09 20:18:29 +0100

    improve to_json methods in proso_flashcards

--- proso_flashcards/apps.py
+++ proso_flashcards/apps.py
@@ -12,8 +12,6 @@ class AppConfig(OAppConfig):
         register_object_type_enricher(['fc_flashcard', 'fc_category', 'fc_term', 'fc_context'], 'proso_models.json_enrich.number_of_answers')
         register_object_type_enricher(['fc_category', 'fc_term', 'fc_context'], 'proso_models.json_enrich.avg_prediction')
         register_object_type_enricher(['fc_context'], 'proso_flashcards.json_enrich.context_flashcards')
-        register_object_type_enricher(['fc_context'], 'proso_flashcards.json_enrich.remove_context_content', priority=-10)
-        register_object_type_enricher(['fc_flashcard'], 'proso_flashcards.json_enrich.remove_flashcard_context', priority=-10)
         register_object_type_enricher(['question'], 'proso_flashcards.json_enrich.answer_type')
         register_object_type_enricher(['question'], 'proso_flashcards.json_enrich.question_type', priority=-1000)
         register_object_type_enricher(['question'], 'proso_flashcards.json_enrich.options', dependencies=['proso_flashcards.json_enrich.question_type'], priority=-1000)
